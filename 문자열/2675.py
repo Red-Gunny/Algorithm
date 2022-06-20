e4a8a34610b7561650534e7038ef0680e31882b3
Red-Gunny <nehepkldi@naver.com>
2022-06-20 20:07:55 +0900

    파일 정리

---
+++ 문자열/2675.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    R,S = input().split()
+    R = int(R)
+
+    for character in S:
+        for _ in range(R):
+            print(character, end='')
+
+    print("")
